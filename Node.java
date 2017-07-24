47921e613a0cfe29d1f43432323c51f46305bf11
raihun <raihun@raihusoft.jp>
2017-07-24 17:04:07 +0900

    Modify 変数名変更

--- Node.java
+++ Node.java
@@ -161,6 +161,7 @@ public class Node {
     return;
   }
 
+  // ルーティングテーブル表示
   public void showRouteList() {
     HashMap<Integer, ArrayList<Integer>> routeList = this.rc.getRouteList();
     System.out.println("--------------------------------------------------");
